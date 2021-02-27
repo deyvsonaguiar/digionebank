a7963ee95ddb797005d84dc53867cc81fdadcb69
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-02-27 19:57:12 -0400

    Criando abstract class Pessoa

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -0,0 +1,15 @@
+package one.digitalinnovation.digionebank
+
+/**
+ * Classe abstrata Pessoa
+ *
+ * Esta classe implementa uma forma abstrata de pessoa
+ *
+ * @property nome O nome da pessoa
+ * @property cpf O cpf da pessoa*/
+
+abstract class Pessoa {
+
+    val nome: String,
+    val cpf: String
+}
