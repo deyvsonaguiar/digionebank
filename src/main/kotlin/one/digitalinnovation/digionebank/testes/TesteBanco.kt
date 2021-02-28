6f6a5d3ce93dbdffffae56a071d4bf79251b155c
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2021-02-27 21:21:21 -0400

    Criando TesteBanco, app quebrou erro "Unresolved referense: copy"

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteBanco.kt
@@ -0,0 +1,24 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Banco
+
+/**
+ * Classe TesteBanco
+ *
+ * Classe que instancia objeto tipo [Banco]
+ */
+
+fun main() {
+    //armazena na variável uma nova instancia da classe herdada Banco
+    val digiOneBank = Banco(nome = "Digione", numero = 5)
+
+    //mostra as informações
+    println(digiOneBank.nome)
+    println(digiOneBank.numero)
+
+    //aplicão quebrou Erro "Kotlin: Unresolved reference: copy"
+    val banco2 = digiOneBank.copy(nome="banco2")
+
+    println(banco2.info())
+
+}
